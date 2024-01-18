d1347d4e8a2d61343d7d512920cfff755921352f
Pavan <pavan.sardjoepersad@gmail.com>
2024-01-18 17:51:05 +0100

    Added dynamic category menu and more Blade components

--- routes/web.php
+++ routes/web.php
@@ -23,11 +23,12 @@ use Illuminate\Support\Facades\Log;
 Route::get('/', function () {
 
     return view('posts', [
-        'posts' => Post::latest()->get()
+        'posts' => Post::latest()->get(),
+        'categories' => Category::all()
     ]);
 
 
-});
+})->name('home');
 
 Route::get('posts/{post:slug}', function(Post $post){
 
@@ -37,13 +38,16 @@ Route::get('posts/{post:slug}', function(Post $post){
 
 Route::get('/categories/{category:slug}', function(Category $category){
     return view('posts', [
-        'posts' => $category->posts
+        'posts' => $category->posts,
+        'currentCategory' => $category,
+        'categories' => Category::all()
     ]);
-});
+})->name('category');
 
 Route::get('/authors/{author:username}', function(User $author){
     return view('posts', [
-        'posts' => $author->posts
+        'posts' => $author->posts,
+        'categories' => Category::all()
     ]);
 });
 
