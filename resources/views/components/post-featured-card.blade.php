433445d0044e30254c75052d080e374135e9953a
Pavan <pavan.sardjoepersad@gmail.com>
2024-01-19 15:06:53 +0100

    Added first version of Search functionality

--- resources/views/components/post-featured-card.blade.php
+++ resources/views/components/post-featured-card.blade.php
@@ -29,10 +29,10 @@
                             </div>
                         </header>
 
-                        <div class="text-sm mt-2">
-                            <p>
-                                {{$post->excerpt}}
-                            </p>
+                        <div class="text-sm mt-2 space-y-4">
+                           
+                                {!!$post->excerpt!!}
+                         
 
                         <footer class="flex justify-between items-center mt-8">
                             <div class="flex items-center text-sm">
